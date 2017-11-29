0347fd2a2c00edebeba00cd65c3a23d2968a0f29
Ji Young Lee <jylee1103@gmail.com>
2017-11-30 06:17:18 +1100

    4 Added electron-updater and Set up Logs to be printed when checkForUpdates is called

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@
 /* eslint-disable global-require */
 import { app } from 'electron';
 import { enableLiveReload } from 'electron-compile';
+import { autoUpdater } from 'electron-updater';
 import appCtx from './context/appCtx';
 import attachedDevTools from './startup/devTools';
 import enableReporter from './startup/reporter';
 import envProps from './context/envProps';
 import window from './startup/window';
+
 
 const config = require('./context/config.json');
 
@@ -38,6 +40,8 @@ app.on('ready', () => {
   aCtx = appCtx(envProps, config);
   const reporter = enableReporter(aCtx);
   const win = popMainWindow(aCtx);
+  autoUpdater.checkForUpdates();
+
   if (aCtx.platform.devMode) {
     enableLiveReload({ strategy: 'react-hmr' });
     attachedDevTools(win, aCtx)
@@ -69,4 +73,32 @@ app.on('activate', () => {
   if (currentWindow === null) {
     popMainWindow(aCtx);
   }
+});
+
+autoUpdater.on('checking-for-update', () => {
+  const msg = 'Checking for update...';
+  aCtx.logger.info(`${msg}`);
+});
+autoUpdater.on('update-available', (info) => {
+  const msg = 'Update available..';
+  aCtx.logger.info(`${msg} ${info}`);
+});
+autoUpdater.on('update-not-available', (info) => {
+  const msg = 'Update not available..';
+  aCtx.logger.info(`${msg} ${info}`);
+});
+autoUpdater.on('error', (err) => {
+  const msg = 'Error in auto-updater..';
+  aCtx.logger.info(`${msg} ${err}`);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let msg = 'Download speed: ' + progressObj.bytesPerSecond;
+  msg = msg + ' - Downloaded ' + progressObj.percent + '%';
+  msg = msg + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  aCtx.logger.info(`${msg} `);
+
+});
+autoUpdater.on('update-downloaded', (info) => {
+  const msg = 'Update downloaded';
+  aCtx.logger.info(`${msg} ${info}`);
 });
